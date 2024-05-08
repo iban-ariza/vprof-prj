ed589ecdaef1ed5c3bc2ec62ebc9e4411e0ad7b7
Iban <iban.ariza@outlook.com>
2024-05-08 14:53:12 -0400

    chore: Add comments to jenkins,nexus,sonar server setup

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
+# Check download/installation page (ubuntu setup)
 sudo apt update
 sudo apt install openjdk-11-jdk -y
 sudo apt install maven -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-  
+
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
--- userdata/sonar-setup.sh
+++ userdata/sonar-setup.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
+
 cp /etc/sysctl.conf /root/sysctl.conf_backup
+
+# Change system configuration (and reboot to apply)
 cat <<EOT> /etc/sysctl.conf
 vm.max_map_count=262144
 fs.file-max=65536
 ulimit -n 65536
 ulimit -u 4096
 EOT
+
 cp /etc/security/limits.conf /root/sec_limit.conf_backup
+
 cat <<EOT> /etc/security/limits.conf
 sonarqube   -   nofile   65536
 sonarqube   -   nproc    409
 EOT
 
+# INSTALL JDK
 sudo apt-get update -y
 sudo apt-get install openjdk-11-jdk -y
 sudo update-alternatives --config java
 
 java -version
 
+# INSTALL POSTGRESQL
 sudo apt update
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
 
@@ -26,16 +33,24 @@ sudo apt install postgresql postgresql-contrib -y
 #sudo -u postgres psql -c "SELECT version();"
 sudo systemctl enable postgresql.service
 sudo systemctl start  postgresql.service
+
+# SETUP USER AND PASSWORD
 sudo echo "postgres:admin123" | chpasswd
+# create user sonar for database
 runuser -l postgres -c "createuser sonar"
+# create password for user sonar in database
 sudo -i -u postgres psql -c "ALTER USER sonar WITH ENCRYPTED PASSWORD 'admin123';"
+# Set new database with owner sonar
 sudo -i -u postgres psql -c "CREATE DATABASE sonarqube OWNER sonar;"
 sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE sonarqube to sonar;"
 systemctl restart  postgresql
 #systemctl status -l   postgresql
 netstat -tulpena | grep postgres
+
+# INSTALL SONAR
 sudo mkdir -p /sonarqube/
 cd /sonarqube/
+# download sonar binaries, unzip and create systemctl for it
 sudo curl -O https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.3.0.34182.zip
 sudo apt-get install zip -y
 sudo unzip -o sonarqube-8.3.0.34182.zip -d /opt/
@@ -44,6 +59,8 @@ sudo groupadd sonar
 sudo useradd -c "SonarQube - User" -d /opt/sonarqube/ -g sonar sonar
 sudo chown sonar:sonar /opt/sonarqube/ -R
 cp /opt/sonarqube/conf/sonar.properties /root/sonar.properties_backup
+
+# specify sonarqube configuration details
 cat <<EOT> /opt/sonarqube/conf/sonar.properties
 sonar.jdbc.username=sonar
 sonar.jdbc.password=admin123
@@ -56,6 +73,7 @@ sonar.log.level=INFO
 sonar.path.logs=logs
 EOT
 
+# create systemctl service
 cat <<EOT> /etc/systemd/system/sonarqube.service
 [Unit]
 Description=SonarQube service
@@ -83,9 +101,12 @@ systemctl daemon-reload
 systemctl enable sonarqube.service
 #systemctl start sonarqube.service
 #systemctl status -l sonarqube.service
+
 apt-get install nginx -y
 rm -rf /etc/nginx/sites-enabled/default
 rm -rf /etc/nginx/sites-available/default
+
+# configure nginx to listen on port 80, and route to port 9000
 cat <<EOT> /etc/nginx/sites-available/sonarqube
 server{
     listen      80;
@@ -101,7 +122,7 @@ server{
         proxy_pass  http://127.0.0.1:9000;
         proxy_next_upstream error timeout invalid_header http_500 http_502 http_503 http_504;
         proxy_redirect off;
-              
+
         proxy_set_header    Host            \$host;
         proxy_set_header    X-Real-IP       \$remote_addr;
         proxy_set_header    X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -109,6 +130,7 @@ server{
     }
 }
 EOT
+
 ln -s /etc/nginx/sites-available/sonarqube /etc/nginx/sites-enabled/sonarqube
 systemctl enable nginx.service
 #systemctl restart nginx.service
